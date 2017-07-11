9b19cd894dc26eefe16730401e9c29a430274e63
nmred <nmred_2008@126.com>
2017-07-11 11:02:02 +0800

    modify cmake

--- src/Version.hpp
+++ src/Version.hpp
@@ -3,8 +3,8 @@
 #define AIDT_VERSION_HPP_
 #define VERSION "1.0.0"
 #define SOVERSION "1"
-#define GIT_DIRTY "311"
-#define GIT_SHA1 "1d2caaa9"
-#define BUILD_ID "localhost.localdomain-1499470789"
+#define GIT_DIRTY "121"
+#define GIT_SHA1 "1f6ec225"
+#define BUILD_ID "bpdev-1499739712"
 #define BUILD_TYPE "Release"
 #endif
