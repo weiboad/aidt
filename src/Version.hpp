0e8cfa01ac3a934eac2f3d2543111baacc269f1b
nmred <nmred_2008@126.com>
2017-07-11 11:07:00 +0800

    update version

--- src/Version.hpp
+++ src/Version.hpp
@@ -1,10 +1,10 @@
 /// 该程序自动生成，禁止修改
 #ifndef AIDT_VERSION_HPP_
 #define AIDT_VERSION_HPP_
-#define VERSION "1.0.0"
+#define VERSION "1.0.1"
 #define SOVERSION "1"
-#define GIT_DIRTY "121"
-#define GIT_SHA1 "1f6ec225"
-#define BUILD_ID "bpdev-1499739712"
+#define GIT_DIRTY "19"
+#define GIT_SHA1 "6d8fcc33"
+#define BUILD_ID "bpdev-1499742228"
 #define BUILD_TYPE "Release"
 #endif
