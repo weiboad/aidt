6a54d81bef0428186c12e88d021d5f190f895a4e
nmred <nmred_2008@126.com>
2017-08-18 17:02:55 +0800

    tag v1.0.5

--- src/Version.hpp
+++ src/Version.hpp
@@ -3,8 +3,8 @@
 #define AIDT_VERSION_HPP_
 #define VERSION "1.0.5"
 #define SOVERSION "1"
-#define GIT_DIRTY "13"
-#define GIT_SHA1 "5068862e"
-#define BUILD_ID "bpdev-1503037663"
+#define GIT_DIRTY "0"
+#define GIT_SHA1 "754c91aa"
+#define BUILD_ID "bpdev-1503037759"
 #define BUILD_TYPE "Release"
 #endif
