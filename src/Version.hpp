754c91aadd9807dd54cfce65720dbd48f907a979
nmred <nmred_2008@126.com>
2017-08-18 14:27:55 +0800

    modify metric

--- src/Version.hpp
+++ src/Version.hpp
@@ -1,10 +1,10 @@
 /// 该程序自动生成，禁止修改
 #ifndef AIDT_VERSION_HPP_
 #define AIDT_VERSION_HPP_
-#define VERSION "1.0.4"
+#define VERSION "1.0.5"
 #define SOVERSION "1"
-#define GIT_DIRTY "0"
-#define GIT_SHA1 "6da0262d"
-#define BUILD_ID "bpdev-1502777046"
+#define GIT_DIRTY "13"
+#define GIT_SHA1 "5068862e"
+#define BUILD_ID "bpdev-1503037663"
 #define BUILD_TYPE "Release"
 #endif
